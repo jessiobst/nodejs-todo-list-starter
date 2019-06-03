ef951ca0f0952184efd94e4f4bc05da7f004d41d
Jessica Obst <32583868+jessiobst@users.noreply.github.com>
2019-06-02 21:58:56 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const tareasRouter = require("./routes/tareas");
+
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/tareas-backend', {useNewUrlParser: true});
+
+app.use(bodyParser.json());
+
+// Redirige las peticiones con URL que terminan en / al equivalente sin la barra final.
+// Ej: http://localhost/tasks/ se redirige a https://localhost/tasks
+app.use((req, res, next) => {
+  const test = /\?[^]*\//.test(req.url);
+  if (req.url.substr(-1) === '/' && req.url.length > 1 && !test)
+    res.redirect(301, req.url.slice(0, -1));
+  else
+    next();
+});
+
+//Aca captura todas las peticiones realizadas por el cliente
+//get tasks y redirige, post tasks y redirige de acuerdo al metodo asignado
+app.use("/tasks",tareasRouter);
+//este metodo vamos a copiar para implementar los demas
+
+
+app.use((req,res,next)=>{
+  try{
+    next()
+  }catch(error){
+    res.status = 500;    
+    res.send({mensaje:'Explotó '+error.message});
+  }
+});
+
+app.use("/",(req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/html");
+  res.end(
+    "<body><title>Tareas REST API</title><h1>API RESTful de Tareas</h1></body>"
+  );
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Servidor escuchando en el host ${hostname} y puerto ${port}`);
+});
